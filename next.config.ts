b4647af2199c2f1724499be7df5c0338daab7865
Beekey <patrykdaros10@gmail.com>
2026-05-23 16:02:07 +0200

    fix prisma url

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   experimental: { workerThreads: false, cpus: 1 },
